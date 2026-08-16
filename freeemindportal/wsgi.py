37d40e0f1594195e77fb2608014526b10deb8383
freemindagency2023-tech <freemindagency2023@gmail.com>
2026-08-16 21:22:15 +0300

    Clean wsgi file

--- freeemindportal/wsgi.py
+++ freeemindportal/wsgi.py
@@ -4,19 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freeemindportal.settings')
 
 application = get_wsgi_application()
-
-# Hii itatengeneza admin yenyewe app ikianza kuwaka mtandaoni
-try:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    username = 'freemindagency2023@gmail.com'
-    password = 'amreen'
-    
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=username, password=password)
-    else:
-        u = User.objects.get(username=username)
-        u.set_password(password)
-        u.save()
-except Exception as e:
-    pass
