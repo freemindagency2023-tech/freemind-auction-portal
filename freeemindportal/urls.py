0109dcc67445b562096521c9953e0365a7f48988
freemindagency2023-tech <freemindagency2023@gmail.com>
2026-08-23 12:57:00 +0300

    Fix logout URL

--- freeemindportal/urls.py
+++ freeemindportal/urls.py
@@ -1,33 +1,73 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.contrib.auth import views as auth_views
+
+from auctions import views
 
 
 urlpatterns = [
 
-    # =========================================================
+    # =====================================================
     # DJANGO ADMIN
-    # =========================================================
+    # =====================================================
 
     path(
-        'admin/',
+        "admin/",
         admin.site.urls
     ),
 
-    # =========================================================
+
+    # =====================================================
     # AUCTIONS APP
-    # =========================================================
+    # =====================================================
 
     path(
-        '',
-        include('auctions.urls')
+        "",
+        include("auctions.urls")
+    ),
+
+
+    # =====================================================
+    # LOGIN
+    # =====================================================
+
+    path(
+        "accounts/login/",
+        views.CustomLoginView.as_view(),
+        name="login",
+    ),
+
+
+    # =====================================================
+    # LOGOUT
+    # =====================================================
+
+    path(
+        "accounts/logout/",
+        auth_views.LogoutView.as_view(
+            next_page="login"
+        ),
+        name="logout",
+    ),
+
+
+    # =====================================================
+    # REGISTER
+    # =====================================================
+
+    path(
+        "accounts/register/",
+        views.register,
+        name="register",
     ),
 ]
 
 
 # =========================================================
-# LOCAL MEDIA / STATIC
+# MEDIA / STATIC - DEVELOPMENT
 # =========================================================
 
 if settings.DEBUG:
